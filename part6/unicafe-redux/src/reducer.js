e6e5d1464582462ca9d0cf75336a25e0333a4b0c
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2022-10-15 15:10:12 +0530

    unicafe-redux

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GOOD":
+      return { ...initialState, good: state.good + 1 };
+    case "OK":
+      return { ...initialState, ok: state.ok + 1 };
+    case "BAD":
+      return { ...initialState, bad: state.bad + 1 };
+    case "ZERO":
+      return { good: 0, ok: 0, bad: 0 };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
