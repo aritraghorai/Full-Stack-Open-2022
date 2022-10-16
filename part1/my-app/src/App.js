9fe17daa39d87245345d5d4da03bc7fc15bade80
Aritra Ghorai <aritraghorai0@gmail.com>
2022-10-16 11:06:49 +0530

    add part 6 Readme.md

--- part1/my-app/src/App.js
+++ part1/my-app/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       },
       {
         name: "Using props to pass data",
-        exercises: 7,
+        exercises: 7
       },
       {
         name: "State of a component",
